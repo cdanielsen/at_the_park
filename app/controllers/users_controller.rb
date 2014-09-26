5304a0109f10b3c00fdecd9197958998c3bd29d3
Christian <student@epicodus.com>
2014-09-26 11:55:02 -0700

    add csrf protection and delete obsolete users controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,9 @@
 class UsersController < ApplicationController
 
-# before_action :authenticate_user!
+before_action :authenticate_user!
 
   def index
     @users = User.all
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      flash[:notice] = "New User '#{@user.name}' created!"
-      redirect_to '/'
-    else
-      render new_user_path
-    end
   end
 
   def show
@@ -25,28 +11,4 @@ class UsersController < ApplicationController
     @parks = Park.all
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      flash[:notice] = "#{@user.name} updated successfully!"
-      redirect_to "/users/#{@user.id}"
-    else
-      render edit_user_path
-    end
-  end
-
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-    flash[:alert] = "User #{@user.name} deleted!"
-    redirect_to '/users'
-  end
-end
-
-def user_params
-  params.require(:user).permit(:name, :email, :password, :age, :gender, :avatar)
 end
